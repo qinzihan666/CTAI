c6376e76de6d86aa71bf56d4dfb7fff39ac87bc9
zihan Qin <150925153+qinzihan666@users.noreply.github.com>
2025-04-04 19:38:25 +0800

    Upload CTAI_web/src/App.vue

---
+++ CTAI_web/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: "直肠肿瘤辅助诊断系统",
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    "app-header": Header,
+  },
+  methods: {
+
+  },
+
+};
+</script>
+
+<style scoped></style>
